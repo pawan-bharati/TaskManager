53d09dba01de9e3bb06e3f3b27bf6256d83c1e6c
pawan_bharati <pawanbharati90@gmail.com>
2020-01-02 08:48:26 +0545

    Initial commit

---
+++ app/src/main/java/com/example/taskmanager/url/Url.java
@@ -0,0 +1,20 @@
+package com.example.taskmanager.url;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class Url {
+
+    public static final String base_url ="http://10.0.2.2:3000/";
+    public static String token ="Bearer";
+    public static String imagePath = base_url + "upload/";
+
+
+    public static Retrofit getInstance() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(base_url)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        return retrofit;
+    }
+}
